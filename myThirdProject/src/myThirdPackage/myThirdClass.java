b4d5760406baac6e476cb70c701d4368f6273b42
Utkarsh Honparkhe <honparkhe01utkarsh@gmail.com>
2019-09-14 19:40:58 +0530

    Publish repository in remote

---
+++ myThirdProject/src/myThirdPackage/myThirdClass.java
@@ -0,0 +1,11 @@
+package myThirdPackage;
+
+public class myThirdClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Publish repository");
+		System.out.println("please update kardo repository");
+	}
+
+}
